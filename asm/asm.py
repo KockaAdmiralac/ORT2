1219e00a64357133f7c473c60625c2dc5310b9ab
topofkeks <28643848+topofkeks@users.noreply.github.com>
2021-01-06 01:27:37 +0100

    Funkcionalnost labela u asembleru.

--- asm/asm.py
+++ asm/asm.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Union
 import sys
 import re
 
@@ -9,8 +9,8 @@ if len(sys.argv) < 2:
 
 # (     0         1               2               3         4          5         6         7     )
 # (Instruction, Indir1, Flags(R, #, +, -), Base1(0b, 0x), Number, Base2(b, h), Indir2, Post(+, -))
-line_regex = re.compile(r'^\s*(HALT|RTS|RTI|ROR|ROTR|RORC|ROTRC|PUSH|POP|BLEQ|BGREU|BNNG|BGRT|JLEQ|JMP|JSR|LD|ST|ADD|INC|OR|STRFIND)\s*(\()?([R#+-])?(0[bx])?([0-9a-f]+)?(h)?(\))?([+-])?\s*$', re.IGNORECASE)
-instructions: List[Tuple[str, Optional[str], Optional[int]]] = []
+line_regex = re.compile(r'^\s*(?:([a-zA-z]+)\s*:)?\s*(HALT|RTS|RTI|ROR|ROTR|RORC|ROTRC|PUSH|POP|BLEQ|BGREU|BNNG|BGRT|JLEQ|JMP|JSR|LD|ST|ADD|INC|OR|STRFIND)\s*(?:(?:(\()?([R#+-])?(0[bx])?([0-9a-f]+)?(h)?(\))?([+-])?)|([a-zA-z]+\s*$))\s*$', re.IGNORECASE)
+instructions: List[Tuple[str, Optional[str], Union[str, int]]] = []
 opcodes: Dict[str, int] = {
     # Bezadresne
     'HALT': 0,
@@ -40,6 +40,36 @@ opcodes: Dict[str, int] = {
     'STRFIND': 53
 }
 
+
+labels : Dict[str, int] = {}
+
+lengths : Dict[str, int] = {
+    None : 0,
+    # Adresiranja
+    'immed' : 4,
+    'memdir' : 4,
+    'memind' : 4,
+    'regind' : 2,
+    'postincr' : 2,
+    'postdec' : 2,
+    'regdir' : 2,
+    # Instrukcije 
+    'HALT' : 1,
+    'RTS' : 1,
+    'RTI' : 1,
+    'ROR' : 1,
+    'RORC' : 1,
+    'PUSH' : 1,
+    'POP' : 1,
+    'BLEQ' : 2,
+    'BGREU' : 2,
+    'BNNG' : 2,
+    'BGRT' : 2,
+    'JLEQ' : 3,
+    'JMP' : 3,
+    'JSR' : 3,
+}
+
 adrcodes = ['immed', 'memdir', 'memind', 'regind', 'postincr', 'postdec', 'regdir']
 
 with open(sys.argv[1], 'r', encoding='utf-8') as asm_file:
@@ -51,14 +81,21 @@ with open(sys.argv[1], 'r', encoding='utf-8') as asm_file:
         if line_match is None:
             print('Sintaksna greška: ', line)
             exit(1)
-        instruction, indir1, flags, base1, number, base2, indir2, post = line_match.groups()
+        label, instruction, indir1, flags, base1, number, base2, indir2, post, destination = line_match.groups()
         instruction = instruction.upper()
         indir = False
         reg = flags == 'R' or flags == 'r'
         immed = flags == '#'
         branch = instruction.startswith('B')
         jump = instruction.startswith('J')
+        if destination is not None and destination.endswith('\n'): destination = destination[:-1] # Uklanjanje \n
         # Validacija
+        if label is not None and label in labels:
+            print('Opet se definiše labela: ',label)
+            exit(1)
+        if destination is not None and not jump:
+            print('Samo instrukcije apsolutnog skoka mogu imati labelu kao operand.')
+            exit(1)
         if flags == '-' and not (branch or immed):
             print('Samo instrukcije grananja i neposredno adresiranje mogu da imaju znak operanda:', line)
             exit(1)
@@ -69,9 +106,14 @@ with open(sys.argv[1], 'r', encoding='utf-8') as asm_file:
         if base1 is not None and base2 is not None:
             print('Zadate su osnove i kao prefiks i kao sufiks:', line)
             exit(1)
+        # Nova labela
+        if label is not None:
+            instructions.append((None,label,None))
         # Određivanje operanda
         operand = None
-        if number is not None:
+        if destination is not None:
+            operand = destination
+        elif number is not None:
             if opcodes[instruction] < 8:
                 print('Bezadresne instrukcije ne primaju operande:', line)
                 exit(1)
@@ -117,8 +159,29 @@ with open(sys.argv[1], 'r', encoding='utf-8') as asm_file:
         # Slanje na formatiranje
         instructions.append((instruction, adr, operand))
 
+# Faza određivanja labela
+tpos = 0x1000
+for instruction, adr, operand in instructions:
+    #print(instruction, adr, operand, tpos)
+    if instruction is None:
+        labels[adr] = tpos
+    else:
+        tpos += lengths.get(instruction, 0) + lengths.get(adr, 0)
+
+# Faza postavljanja adresa labela
+for i, (instruction, adr, operand) in enumerate(instructions):
+    #print(instruction, adr, operand, i)
+    if isinstance(operand, str):
+        if operand not in labels:
+            print('Nedefinisana labela: ', operand)
+            exit(1)
+        instructions[i] = (instruction, adr, labels[operand])
+
+# Štampanje
 for instruction, adr, operand in instructions:
     # print(instruction, adr, operand)
+    if instruction is None:
+        continue
     opcode = opcodes[instruction]
     instruction_format = ['{:08b}'.format(opcode)]
     if instruction.startswith('J'):
---
+++ asm/test2.asm
@@ -0,0 +1,7 @@
+start: LD 32
+JMP batak
+batak: JSR kurcinela
+HALT
+HALT
+HALT
+kurcinela: JLEQ start
