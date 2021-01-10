122c273285fae83702f9ae23c95a43535975038e
Luka Simić <git@kocka.tech>
2021-01-11 00:12:24 +0100

    ADDR and EXEC fixes; ADD works.

--- asm/test1.asm
+++ asm/test1.asm
@@ -1,6 +1,6 @@
         LD #0x1000   ; Učitava se 1000h u akumulator               N = 0, Z = 0, C = 0, V = 0
         ST 2000h     ; Upisuje se 1000h na adresu 2000h            N = 0, Z = 0, C = 0, V = 0
-        ADD (2000h)  ; Dodaje se 1000h na akumulator, A = 2000h    N = 0, Z = 0, C = 0, V = 0
+        ADD (2000h)  ; Dodaje se 3000h na akumulator, A = 2000h    N = 0, Z = 0, C = 0, V = 0
         PUSH         ; Gura se 2000h na stek                       N = 0, Z = 0, C = 0, V = 0
         ST #3        ; Postavlja akumulator na 3                   N = 0, Z = 0, C = 0, V = 0
         ADD #-1      ; Smanjuje akumulator za 1                    N = ?, Z = ?, C = 0, V = 0
